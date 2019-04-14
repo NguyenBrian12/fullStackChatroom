5cb63b4b412ac3610a54f87fd8484b360632ba79
Brian Nguyen <nguyenbrian12@yahoo.com>
2019-04-14 15:18:08 -0700

    build

---
+++ chatroom/build/precache-manifest.b06c88291701470813cdfef5f79519c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "04b78ebb9dade67fb34e",
+    "url": "/static/js/main.04b78ebb.chunk.js"
+  },
+  {
+    "revision": "224b5785c6e77b76a988",
+    "url": "/static/js/1.224b5785.chunk.js"
+  },
+  {
+    "revision": "04b78ebb9dade67fb34e",
+    "url": "/static/css/main.40a8d113.chunk.css"
+  },
+  {
+    "revision": "4d3e4b83960a1130a65d6858ce8bb89e",
+    "url": "/index.html"
+  }
+];
